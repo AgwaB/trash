4e87066eaea173b704a9280addd57836a8af522a
agwab <leesd556@gmail.com>
2024-12-16 20:54:51 +0900

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,14 @@ import DesktopLayout from '@/components/desktop/layout/DesktopLayout'
 import MobileLayout from '@/components/mobile/layout/MobileLayout'
 
 export default function Home() {
-  const isMobileDevice = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-  return isMobileDevice ? <MobileLayout /> : <DesktopLayout />
+  return (
+    <>
+      <div className="hidden md:block">
+        <DesktopLayout />
+      </div>
+      <div className="block md:hidden">
+        <MobileLayout />
+      </div>
+    </>
+  )
 }
